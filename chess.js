f33f07cc75d175d4727a534b833ecab7a75a21b4
TruthsSeeker <heinrichloic@gmail.com>
2017-03-02 19:31:43 +0100

    Done

--- chess.js
+++ chess.js
@@ -5,8 +5,9 @@ $( document ).ready(function() {
     var turn = 0;
 
     /*
-    I am reworking this function to reflect the changes made to isTurn.php and to clarify the logic.
-    Check turn will actually check whether the turn is the same.
+     *I am reworking this function to reflect the changes made to isTurn.php and to clarify the logic.
+     *Check turn will actually check whether the turn is the same.
+     * */
     
     function checkTurn(){
         setTimeout(function(){
@@ -26,17 +27,16 @@ $( document ).ready(function() {
                             checkTurn();
                         }
                     }
-                )};//TODO arretez de vous rafraichir (vous etes des animaux)
-        }, 500)
+                )};
+        }, 2000)
 
-    }*/
+    }
 //I tried something by separating the logic of pieceMove() from the JQuery event function but something isn't
 //working correctly. I suspect I'm doing something wrong regarding jQuery events or I need to pass parameters
-//TODO pass $(this) to pieceMove()
-    function pieceMove() {
+    function pieceMove(td) {
         if (step) {
-            targetCoordinates = $(this).attr("data-id").split(";");//chopper coordonnées d'arrivée
-            piece.children().appendTo(this);
+            targetCoordinates = td.attr("data-id").split(";");//chopper coordonnées d'arrivée
+            piece.children().appendTo(td);
 
 
             $.post(
@@ -53,8 +53,8 @@ $( document ).ready(function() {
             );
         }
         if (!step) {
-            startCoordinates = $(this).attr("data-id").split(";");//chopper coordonnées de départ
-            piece = $(this);
+            startCoordinates = $(td).attr("data-id").split(";");//chopper coordonnées de départ
+            piece = $(td);
             if (piece.children().data('color') == $('.info').data('player') && $('.info').data('player') == $('.info').data('turn')) {
                 step = !step;
             }
@@ -67,15 +67,16 @@ $( document ).ready(function() {
         }
     }
 
-    /*checkTurn();*/
-
+    if ($('.info').data('turn') != $('.info').data('player')){
+    checkTurn();
+    }
 
 //This is the "event function". I was trying to see the different things happening for each event
 //but there is something I'm doing wrong. I think it might be to do with the parameters I'm passing
 //to pieceMove()
-$('td').click(function () {
-    pieceMove();
-});
+    $('td').click(function () {
+        pieceMove($(this));
+    });
 
 });
 
